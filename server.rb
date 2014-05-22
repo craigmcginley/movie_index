97d46be53d59af5956c4599c50b2851035a9cef1
craigmcginley <craigmcginley@gmail.com>
2014-05-21 22:07:15 -0400

    Cleaned up and extracted logic to methods

--- server.rb
+++ server.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'csv'
+require 'pry'
 
 def get_movie_data
   movies = []
@@ -9,50 +10,76 @@ def get_movie_data
   movies.sort_by! {|k| k[:title]}
 end
 
+def search(search, movies)
+  return [] if search == ""
+  search_results = []
+  movies.each do |movie|
+    if movie[:title].downcase.match(/\b#{search}\b/)
+      search_results << movie
+    elsif movie[:synopsis] == "" || movie[:synopsis] == nil
+    elsif movie[:synopsis].downcase.match(/\b#{search}\b/)
+      search_results << movie
+    end
+  end
+  search_results
+end
+
+def display(page, movies)
+  movies[(20*(page-1))..(20*(page-1)+19)]
+end
+
+def max_length(movies)
+  max_length = 0
+  if movies.length % 20 == 0
+    max_length = movies.length / 20
+  else
+    max_length = ((movies.length / 20) + 1)
+  end
+  max_length
+end
+
+get '/' do
+  redirect '/movies'
+end
+
 get '/movies' do
-  page = params[:page].to_i
-  search = params[:query].to_s.downcase
+  page = 1
+  if params.has_key?("page")
+    if params["page"] == ""
+      page = 1
+    else
+      page = params["page"].to_i
+    end
+  end
+
+  search = nil
+  if params.has_key?("query")
+    search = params["query"].to_s.downcase
+  end
+
   @movies = get_movie_data
   @display_movies = []
-  @no_results = ""
-  @search_movies = []
+  @max_length = max_length(@movies)
 
-  if @movies.length % 20 == 0
-    @max_length = (@movies.length/20)
-  else
-    @max_length = ((@movies.length/20) + 1)
-  end
-  if params[:page] == nil
-    page = 1
+  @display_movies = display(page, @movies)
+
+  if search != nil
+    @display_movies = search(search, @movies)
   end
 
-  @display_movies = @movies[(20*(page-1))..(20*(page-1)+19)]
-
-  if search == ""
-  else
-    @movies.each do |movie|
-      if movie[:title].downcase.match(/\b#{search}\b/)
-        @search_movies << movie
-      elsif movie[:synopsis] == "" || movie[:synopsis] == nil
-      elsif movie[:synopsis].downcase.match(/\b#{search}\b/)
-        @search_movies << movie
-      else
-        @no_results = "***No search results***"
-      end
-    end
-    @display_movies = @search_movies[(20*(page-1))..(20*(page-1)+19)]
-  end
-
-  erb :index
+  erb :index2
 end
 
 get '/movies/:id' do
   @movies = get_movie_data
+  @max_length = max_length(@movies)
+
   @movie = nil
   @movies.each do |movie|
     if movie[:id] == params[:id]
       @movie = movie
     end
   end
-  erb :id
+
+  erb :id2
 end
